cd7738fb814b36fe03b7c4a113fe5c092711abf2
PasupathiNadh <pasupathinadhff@gmail.com>
2026-04-14 09:49:10 +0530

    Script

---
+++ script1.js
@@ -0,0 +1,6 @@
+
+// console.log(document);
+// console.log(document.body);
+let  object= document.getElementById("object");
+object.innerText="JavaScript";
+//object.innerHTML="<b>JavaScript</b>";
---
+++ script2.js
@@ -0,0 +1,6 @@
+let boxes= document.getElementsByClassName("box");
+let para=document.getElementsByTagName("para");
+  boxes[1].innerHTML="Second Box";
+ para[0].innerHTML="This is  First Paragraph Holaa";
+
+
