aff23cbed29cf517a205fe33afd8944caae4cc5f
PasupathiNadh <pasupathinadhff@gmail.com>
2026-04-15 09:21:34 +0530

    JS

---
+++ Script3.js
@@ -0,0 +1,6 @@
+const myFun = () =>{
+  //console.log("Pasupathi Nadh");
+  let display=document.getElementById("display");
+  display.innerHTML=display.innerHTML=="Hey"?"Hello":"Hey"
+  display.style.color="Blue"
+};
