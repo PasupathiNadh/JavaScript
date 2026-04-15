c3de1e4ff66292648a75ad12b967315464270a73
PasupathiNadh <pasupathinadhff@gmail.com>
2026-04-15 09:46:23 +0530

    Js Project Files in Class

---
+++ project1.js
@@ -0,0 +1,13 @@
+let isShow=true
+const myFun = () =>{
+  let display=document.getElementById("display")
+  if(isShow)
+  {
+    imgaddress.src="https://img.freepik.com/free-photo/lavender-field-sunset-near-valensole_268835-3910.jpg?semt=ais_hybrid&w=740&q=80"
+    isShow=!isShow
+  }
+  else  {
+    imgaddress.src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630"
+    isShow=!isShow
+  }
+}
