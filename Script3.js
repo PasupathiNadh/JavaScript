152af09d0cb9dbef2945f82c70ec936aabe34532
PasupathiNadh <pasupathinadhff@gmail.com>
2026-04-15 09:31:58 +0530

    By Using if-else Condition

--- Script3.js
+++ Script3.js
@@ -1,6 +1,18 @@
+let c=0;
 const myFun = () =>{
   //console.log("Pasupathi Nadh");
   let display=document.getElementById("display");
-  display.innerHTML=display.innerHTML=="Hey"?"Hello":"Hey"
-  display.style.color="Blue"
+ // display.innerHTML=display.innerHTML=="Hey"?"Hello":"Hey"
+ if(c%2==0)
+ {
+     display.innerHTML="Hii";
+    display.style.color="Blue"
+ }
+ else
+ {
+    display.innerHTML="Hello"
+    display.style.color="Red"
+ }
+ c++;
+  
 };
